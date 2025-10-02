d0f952ddb183976b91ea0a52ce5aee1865d80146
Luan Deivid <luandeivid506@gmail.com>
2025-10-02 14:11:19 -0300

    estrutura de dados

---
+++ public/app.js
@@ -0,0 +1,23 @@
+const carros = [
+  {
+    "id": 1,
+    "nome": "Speedster X",
+    "descricao": "Motor V8 500cv, performance máxima.",
+    "detalhes": "O Speedster X é equipado com um motor V8 de 500cv, tração integral e câmbio automático de 7 marchas. Ideal para quem busca desempenho e emoção.",
+    "imagem": "Porsche-1.jpg"
+  },
+  {
+    "id": 2,
+    "nome": "Urban SUV",
+    "descricao": "Conforto e robustez para viagens.",
+    "detalhes": "O Urban SUV combina tecnologia e segurança, com espaço interno amplo, motor turbo eficiente e sistema de entretenimento de última geração.",
+    "imagem": "urban.jpg"
+  },
+  {
+    "id": 3,
+    "nome": "EcoDrive",
+    "descricao": "100% elétrico, autonomia de 600km.",
+    "detalhes": "O EcoDrive é o futuro da mobilidade. Sustentável, com recarga rápida e autonomia de até 600 km, além de tecnologia de direção semiautônoma.",
+    "imagem": "eco.jpg"
+  }
+];
